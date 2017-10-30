bc532edc3767600ee6344475198740f0d2693718
Steve Mussmann <smussmann@gmail.com>
2017-10-30 10:52:50 -0500

    added command line option parsing

--- bayesass_sum.pl
+++ bayesass_sum.pl
@@ -2,7 +2,27 @@
 
 use warnings;
 use strict;
+use Getopt::Std;
+use Data::Dumper;
 
+# kill program and print help if no command line arguments were given
+if( scalar( @ARGV ) == 0 ){
+  &help;
+  die "Exiting program because no command line options were used.\n\n";
+}
+
+# take command line arguments
+my %opts;
+getopts( 'hd:o:', \%opts );
+
+# if -h flag is used, or if no command line arguments were specified, kill program and print help
+if( $opts{h} ){
+  &help;
+  die "Exiting program because help flag was used.\n\n";
+}
+
+# parse the command line
+my( $dir, $out ) = &parsecom( \%opts );
 
 my $file = "spd_182+DES+BBC.out";
 
@@ -11,32 +31,62 @@ my $file = "spd_182+DES+BBC.out";
 exit;
 
 
+#####################################################################################################
+############################################ subroutines ############################################
+#####################################################################################################
+# subroutine to print help
+sub help{
+  
+  print "\nbayesass_sum.pl is a perl script developed by Steven Michael Mussmann\n\n";
+  print "To report bugs send an email to mussmann\@email.uark.edu\n";
+  print "When submitting bugs please include all input files, options used for the program, and all error messages that were printed to the screen\n\n";
+  print "Program Options:\n";
+  print "\t\t[ -h | -d | -o ]\n\n";
+  print "\t-h:\tDisplay this help message.\n";
+  print "\t\tThe program will die after the help message is displayed.\n\n";
+  print "\t-m:\tSpecify your directory containing bayesass output files.\n";
+  print "\t\tIf no directory is provided, program will exit.\n\n";
+  print "\t-o:\tSpecify the output file name.\n";
+  print "\t\tIf no name is provided, the program will exit.\n\n";
+  
+}
+
+#####################################################################################################
+# subroutine to parse the command line options
+
+sub parsecom{ 
+  
+	my( $params ) =  @_;
+	my %opts = %$params;
+  
+	# set default values for command line arguments
+	my $dir = $opts{d} || die "No input directory specified.\n\n"; #used to specify input directory
+	my $out = $opts{o} || die "No output file specified.\n\n"  ; #used to specify output file name.
+
+	return( $dir, $out );
+
+}
 
 #####################################################################################################
 # subroutine to put file into an array
 
 sub filetoarray{
 
-  my( $infile, $array ) = @_;
+	my( $infile, $array ) = @_;
 
   
-  # open the input file
-  open( FILE, $infile ) or die "Can't open $infile: $!\n\n";
+	# open the input file
+	open( FILE, $infile ) or die "Can't open $infile: $!\n\n";
 
-  # loop through input file, pushing lines onto array
-  while( my $line = <FILE> ){
-    chomp( $line );
-    next if($line =~ /^\s*$/);
-    #print $line, "\n";
-    push( @$array, $line );
-  }
+	# loop through input file, pushing lines onto array
+	while( my $line = <FILE> ){
+		chomp( $line );
+		next if($line =~ /^\s*$/);
+		push( @$array, $line );
+	}
 
-  #foreach my $thing( @$array ){
-  #	print $thing, "\n";
-  #}
-
-  # close input file
-  close FILE;
+	# close input file
+	close FILE;
 
 }
 
